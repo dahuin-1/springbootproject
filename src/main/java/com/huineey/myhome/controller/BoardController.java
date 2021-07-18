1384b1c43ea76696f14a2a5f334f05faa30e4a1f
dahuin-1 <dawhite01@naver.com>
2021-07-18 15:04:17 +0900

    update search by content and title

--- src/main/java/com/huineey/myhome/controller/BoardController.java
+++ src/main/java/com/huineey/myhome/controller/BoardController.java
@@ -26,8 +26,10 @@ public class BoardController {
     private BoardValidator boardValidator;
 
     @GetMapping("/list")
-    public String list(Model model, @PageableDefault(size = 2) Pageable pageable) { //데이터값을 추가하고 싶을 때 파라미터로 모델 넘김
-        Page<Board> boards = boardRepository.findAll(pageable);
+    public String list(Model model, @PageableDefault(size = 2) Pageable pageable,
+                       @RequestParam(required = false, defaultValue = "") String searchText) { //데이터값을 추가하고 싶을 때 파라미터로 모델 넘김
+     //   Page<Board> boards = boardRepository.findAll(pageable);
+        Page<Board> boards = boardRepository.findByTitleContainingOrContentContaining(searchText, searchText, pageable);
         int startPage = Math.max(1, boards.getPageable().getPageNumber() - 4);
         int endPage = Math.min(boards.getTotalPages(), boards.getPageable().getPageNumber() + 4);
         model.addAttribute("startPage", startPage);
--- src/main/java/com/huineey/myhome/repository/BoardRepository.java
+++ src/main/java/com/huineey/myhome/repository/BoardRepository.java
@@ -1,6 +1,8 @@
 package com.huineey.myhome.repository;
 
 import com.huineey.myhome.model.Board;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -8,5 +10,7 @@ import java.util.List;
 public interface BoardRepository extends JpaRepository<Board, Long> {
     List<Board> findByTitle(String title);
     List<Board> findByTitleOrContent(String title, String content);
+
+    Page<Board> findByTitleContainingOrContentContaining(String title, String content, Pageable pageable);
 
 }
