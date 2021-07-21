ec6044580dccbf6d8522d38bef2215c6d9241a3c
dahuin-1 <dawhite01@naver.com>
2021-07-21 23:52:21 +0900

    add Role

--- src/main/java/com/huineey/myhome/model/Role.java
+++ src/main/java/com/huineey/myhome/model/Role.java
@@ -1,24 +1,18 @@
 package com.huineey.myhome.model;
 
 import lombok.Data;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import java.util.List;
-
 
 @Entity
 @Data
-public class User {
+public class Role {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String username;
-    private String password;
-    private Boolean enabled;
-
-    //private List<String> roles;
-
+    private String name;
 }
