1908d9f498c14884bb2e87ebd8a72d9688935334
dahuin-1 <dawhite01@naver.com>
2021-07-27 17:47:41 +0900

    modify board.java to change user name

--- src/main/java/com/huineey/myhome/model/Board.java
+++ src/main/java/com/huineey/myhome/model/Board.java
@@ -1,12 +1,10 @@
 package com.huineey.myhome.model;
 
 import lombok.Data;
+
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.validation.constraints.Size;
 
 
@@ -20,4 +18,9 @@ public class Board {
     @Size(min=2, max=30, message = "제목은 2자이상 30자 이하입니다")
     private String title;
     private String content;
+
+    @ManyToOne //게시글 입장에서는 매니 투 원
+    @JoinColumn(name = "user_id", referencedColumnName = "id") //어떤 칼럼과 유저 테이블이 연결이 될지
+    private User user;
+
 }
