a7a0189a4d2c146980895b76fef34fbbdf4f5e69
dahuin-1 <dawhite01@naver.com>
2021-09-20 15:49:32 +0900

    update

--- src/main/java/com/huineey/myhome/controller/CommentController.java
+++ src/main/java/com/huineey/myhome/controller/CommentController.java
@@ -29,14 +29,6 @@ public class CommentController {
     @Autowired
     CommentRepository commentRepository;
 
-    //  @CrossOrigin(origins = "*", allowedHeaders = "*")
- /*   @GetMapping("/comments/{id}")
-    List<Comment> getBoardComments(@PathVariable Long id) {
-        System.out.println("hello");
-        Board post = boardRepository.findById(id).get();
-        return commentRepository.findCommentsByBoard(post);
-    }*/
-
     @GetMapping("/comments")
     List<Comment> all(@RequestParam(required = false, defaultValue = "") String content) {
         if (StringUtils.isEmpty(content)) {
@@ -57,43 +49,3 @@ public class CommentController {
         return commentRepository.save(newComment);
     }
 }
-
-  /*  @GetMapping("/boards/{id}")
-    public String list(Model model,@PathVariable Long id, @PathVariable Long commentID) {
-        //데이터값을 추가하고 싶을 때 파라미터로 모델 넘김
-        // Page<Board> boards = boardRepository.findAll(pageable);
-        Board post = boardRepository.findById(id).get();
-        Comment newComment = commentRepository.findById(commentID).get();
-        model.addAttribute("post", post);
-        return "/boards/"+ id;
-    }*/
-
-
-   /* @CrossOrigin(origins = "*", allowedHeaders = "*")
-    @PutMapping("/boards/{id}/comment")
-    public Comment createComment(@PathVariable Long id, @RequestBody Comment comment) {
-        Optional<Comment> commentItem = commentRepository.findById(id);
-        comment.setContent(commentItem.get());
-        commentRepository.save(comment);
-        return comment;
-    }
-
-    @CrossOrigin(origins = "*", allowedHeaders = "*")
-    @PutMapping("/boards/{id}/comment/{commentID}")
-    public Comment updateComment(@PathVariable Long id, @PathVariable Long commentID, @RequestBody Comment comment) {
-        Optional<Board> boardItem = boardRepository.findById(id);
-        comment.setBoard(boardItem.get());
-        Comment newComment = commentRepository.findById(commentID).get();
-        newComment.setContent(comment.getContent());
-        newComment.setWriter(comment.getWriter());
-        return newComment;
-    }
-
-    @CrossOrigin(origins = "*", allowedHeaders = "*")
-    @PutMapping("/boards/{id}/comment/{commentID}")
-    public String createComment(@PathVariable Long id, @PathVariable Long commentID) {
-        commentRepository.deleteById(commentID);
-        return "Comment delete success";
-    }*/
-
-
